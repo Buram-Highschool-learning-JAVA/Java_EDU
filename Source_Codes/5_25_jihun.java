4a22e13a9797d3f30e9ee5f2d0a3fac8d6a654ac
UROUS3814 <urous3814@gmail.com>
2022-05-25 14:58:14 +0900

    Create 5_25_jihun.java

---
+++ Source_Codes/5_25_jihun.java
@@ -0,0 +1,15 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        switch(a){
+            case "jihun":   System.out.println("mine");
+                    break;
+            case "hi":      System.out.println("xxx");
+                    break;
+        }
+    }
+}
